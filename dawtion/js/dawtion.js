499f90175167d5904b25f91b22e3a0647208b736
Thijs Boom <twboom@hotmail.nl>
2021-03-23 00:51:40 +0100

    Started the development

--- dawtion/js/dawtion.js
+++ dawtion/js/dawtion.js
@@ -1,1 +1,13 @@
 dawtion = [];
+dawtion.version = 'InDev, before publish';
+
+dawtion.init = function() {
+    /* Creating elements in the DOM */
+    // Version display in the header
+    const headerVersionDisplay = document.createElement('p');
+    headerVersionDisplay.setAttribute('id', 'header-version-display');
+    headerVersionDisplay.innerHTML = `Dawtion Version: ${dawtion.version}`;
+
+    // Appending the elements to the document
+    document.getElementById('header').appendChild(headerVersionDisplay)
+};
